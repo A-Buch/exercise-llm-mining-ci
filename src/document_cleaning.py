e11d5ae92ac915ff45b2f023139d745075b7210e
a-buch <anna.buch@tu-berlin.de>
2026-08-25 20:28:04 +0200

    refactor: smaller tidy-ups

--- src/document_cleaning.py
+++ src/document_cleaning.py
@@ -5,15 +5,15 @@
 import re
 
 
-def add(a,b):
-    return (a+b)
+def add(a, b):
+    return a + b
 
 
 def extract_citation_info(citation_text: str) -> str:
     """Extract citation information from the document name."""
-    
-    citation_pattern = r"(.*?)(\d{4})(.*)" # split at first occurrence of year
-    authors, year, title = re.findall(citation_pattern, citation_text)[0] 
+
+    citation_pattern = r"(.*?)(\d{4})(.*)"  # split at first occurrence of year
+    authors, year, title = re.findall(citation_pattern, citation_text)[0]
     authors = authors.replace("et al ", "")
     title = title.replace(" - ", "")
 
--- src/utils.py
+++ src/utils.py
@@ -1,47 +1,46 @@
 # standard library
-import os
 import requests
 import urllib
 
-import torch
-
 
 def calc_recall(tps_no: int, fns_no: int):
-    return tps_no / (tps_no + fns_no) 
+    return tps_no / (tps_no + fns_no)
+
 
 def calc_precision(tps_no: int, fps_no: int):
-    return tps_no / (tps_no + fps_no) 
+    return tps_no / (tps_no + fps_no)
+
 
 def calc_f1(recall: int, precision: int):
     return 2 * (precision * recall) / (precision + recall)
 
 
-
 class Gazetteer:
-
-    def __init__(self, gazetteer_source:str='nominatm'):
-
+    def __init__(self, gazetteer_source: str = "nominatm"):
         self.gazetteer_source = gazetteer_source
         self.base_url = "https://nominatim.openstreetmap.org/"
         self.session = requests.Session()
         self.cache = {}
-    
 
-    def nominatim_query(self, location:str) -> dict:
-
+    def nominatim_query(self, location: str) -> dict:
         # build url
-        location = urllib.parse.quote(location) 
-        url_metainfo = self.base_url + f'search?q={location}&format=geocodejson'
-        url_metainfo += '&polygon_geojson=1&addressdetails=1&namedetails=1&accept-language=en'
-            
+        location = urllib.parse.quote(location)
+        url_metainfo = self.base_url + f"search?q={location}&format=geocodejson"
+        url_metainfo += (
+            "&polygon_geojson=1&addressdetails=1&namedetails=1&accept-language=en"
+        )
+
         # API call
-        r_metainfo = self.session.get(url_metainfo, timeout=20, headers={'User-agent':None}) 
-        
+        r_metainfo = self.session.get(
+            url_metainfo, timeout=20, headers={"User-agent": None}
+        )
+
         # Example: check if the API request was successful
-        assert r_metainfo.status_code == 200, f"Unexpected status code: {r_metainfo.status_code}"
-        
+        assert (
+            r_metainfo.status_code == 200
+        ), f"Unexpected status code: {r_metainfo.status_code}"
 
         r_metainfo_json = r_metainfo.json()
-        self.cache.update({'query':r_metainfo_json})
-        
-        return r_metainfo_json 
+        self.cache.update({"query": r_metainfo_json})
+
+        return r_metainfo_json
