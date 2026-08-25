e3d26d597b549d5922d704357c707da98fb8d939
a-buch <anna.buch@tu-berlin.de>
2026-08-25 20:07:34 +0200

    refactor: tidy-up with pre-commit hooks :)

--- tests/src/test_utils.py
+++ tests/src/test_utils.py
@@ -1,28 +1,20 @@
 # standard library
-import os
-import sys
 import requests
-from typing import Optional
-import urllib
 import unittest
-from unittest.mock import patch   
+from unittest.mock import patch
 
 # third party imports
 import requests_mock
-import pandas as pd
-import torch
 import numpy as np
 
 # local imports
 from src import utils as u
 
 
-
 @requests_mock.mock()
 def mock_request(m):
-    m.get(url, text='success')
+    m.get(url, text="success")
     return requests.get(url).text
-
 
 
 def test_metrics():
@@ -31,23 +23,22 @@ def test_metrics():
     assert np.round(u.calc_f1(0.8, 0.8), 1) == 0.8
 
 
-
-
 class TestGazetteer(unittest.TestCase):
-
     def setUp(self):
-        self.gaz = u.Gazetteer(gazetteer_source='nominatim')
-    
-    @requests_mock.mock()  
-    def test_nominatim_query_calls(self, m):
+        self.gaz = u.Gazetteer(gazetteer_source="nominatim")
 
+    @requests_mock.mock()
+    def test_nominatim_query_calls(self, m):
         # set up mock for nominatim api
         # take any location in OSM as dummy that exists in really,eg. Saxony, the only need is to check if nominatim_query() works
         url = "https://nominatim.openstreetmap.org/search?q=Saxony&format=geocodejson&polygon_geojson=1&addressdetails=1&namedetails=1&accept-language=en"
         json_out = '[{"name":"test"}]'
         m.get(url, text=json_out)
         print(m.get(url, text=json_out))
-        with patch.object(self.gaz, 'nominatim_query', wraps=self.gaz.nominatim_query) as mock_nom_query:
-            _ = self.gaz.nominatim_query('Saxony')   # test mocked nominatim_query() with our Dummy Location "Saxony"; it should give same response as for variable "url"
+        with patch.object(
+            self.gaz, "nominatim_query", wraps=self.gaz.nominatim_query
+        ) as mock_nom_query:
+            _ = self.gaz.nominatim_query(
+                "Saxony"
+            )  # test mocked nominatim_query() with our Dummy Location "Saxony"; it should give same response as for variable "url"
             mock_nom_query.assert_called()
-
